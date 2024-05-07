c89e736518b0d6c6a9ab260e78f66ddd91f77aef
tKarnigt <jinokrit.t@gmail.com>
2024-05-07 15:56:15 +0700

    content in index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const plusHandler = (req, res) => {
 app.get("/", (req, res) => res.send("Hello World!"))
 app.get("/secret", (req, res) => res.send("This is SECRET! I won't tell you :P"))
 app.get("/plus/:num1/:num2",plusHandler)
-const server = app.listen(3001, () => console.log("Application is listening on port 3001!"))
+const server = app.listen(3001, '0.0.0.0', () => console.log("Application is listening on port 3001!"))
 
 module.exports = {
     plusHandler,
